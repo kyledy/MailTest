c7a94db2be6f6a90f646a119632ecba966f4c090
Kyle Dy <kylematthewdy0104@gmail.com>
2023-03-03 14:22:01 -0800

    Update email-controller.js

--- src/controllers/email-controller.js
+++ src/controllers/email-controller.js
@@ -1,5 +1,3 @@
 /**
  * This file handles HTTP requests related to email operations, such as sending emails and retrieving email metadata.
  */
-const { service, auth } = require("../config/email-config");
-const nodemailer = require("nodemailer");
