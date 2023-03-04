3350820607edb617b27306c92af40c8faeeb00bc
Kyle Dy <kylematthewdy0104@gmail.com>
2023-03-04 10:55:27 -0800

    nodemailer down, working on email services first

--- src/config/email-config.js
+++ src/config/email-config.js
@@ -2,8 +2,8 @@
  * This file contains configuration settings for our email provider.
  */
 
-module.exports.service = "gmail";
+module.exports.service = "smtp"; // will most likely change to Exchange ActiveSync (used by UBC mail)
 module.exports.auth = {
-  user: "kylematthewdy0104@gmail.com",
-  pass: "fntjysevrygnxeaq",
+  user: "your-username@gmail.com",
+  pass: "your-password@gmail.com",
 };
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -2,3 +2,5 @@
  * This file contains functions that implement the business logic for email operations, such as sending and receiving
  * emails, parsing email content, and validating email addresses.
  */
+
+const Email = require("../models/email-model.js");
